0662b7a2d82e7af3e8eefa1b9fc4f54cd80ee0af
Mananveer Singh <74530708+manan17890@users.noreply.github.com>
2022-07-02 13:32:50 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCi7Xkp_PtEk8XfR7WcrRoUhbFZhUIy48I",
+  authDomain: "project-85-15b31.firebaseapp.com",
+  databaseURL: "https://project-85-15b31-default-rtdb.firebaseio.com",
+  projectId: "project-85-15b31",
+  storageBucket: "project-85-15b31.appspot.com",
+  messagingSenderId: "298116548739",
+  appId: "1:298116548739:web:9f670e6e2d0f83864e33b6"
+};
